d7acf601c8f160f11b8dc5a048225e4d702e48aa
LarsEsDoch <lars.bogatz@gmail.com>
2025-06-05 22:55:14 +0200

    Added fireball class/object

---
+++ fireball.py
@@ -0,0 +1,35 @@
+from resources import FIREBALL_FRAMES, clock, screen, logging, pygame
+
+
+class Fireball():
+    def __init__(self, y):
+        self.x = 200
+        self.y = y
+        self.frames = FIREBALL_FRAMES
+        self.animation_timer = 0
+        self.current_frame = 0
+        self.masks = [pygame.mask.from_surface(image) for image in self.frames]
+
+    def update(self):
+        self.x += 1
+        self.animation_timer += clock.get_time()
+        self.current_frame = int(self.animation_timer / 100) % len(self.frames)
+
+    def draw(self):
+        screen.blit(self.frames[self.current_frame], (self.x, self.y))
+        if logging.getLogger().getEffectiveLevel() == logging.DEBUG:
+            mask_surface = self.masks[self.current_frame].to_surface()
+            mask_surface.set_colorkey((0, 0, 0))
+            screen.blit(mask_surface, (self.x, self.y))
+
+    def collides_with(self, obstacle):
+        if obstacle.type == "double":
+            obstacle_1 = obstacle.mask.overlap(self.masks[self.current_frame], (self.x - self.x, self.y - self.y[0]))
+            obstacle_2 = obstacle.mask.overlap(self.masks[self.current_frame],
+                                           (self.x - self.x - obstacle.width[0] - 10, self.y - self.y[1]))
+            return obstacle_1 is not None or obstacle_2 is not None
+        elif obstacle.type == "bird":
+            return obstacle.masks[obstacle.current_frame].overlap(self.masks[self.current_frame],
+                                                          (self.x - self.x, self.y - self.y[0])) is not None
+        else:
+            return obstacle.mask.overlap(self.masks[self.current_frame], (self.x - self.x, self.y - self.y[0])) is not None
