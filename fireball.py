77bf31f00c58d1b4ad03ca00bc2d956e9e419751
LarsEsDoch <lars.bogatz@gmail.com>
2025-06-06 17:44:13 +0200

    Fixed fireball collisions, init y and update x

--- fireball.py
+++ fireball.py
@@ -5,7 +5,7 @@ from resources import FIREBALL_FRAMES, clock, screen, logging, pygame
 class Fireball():
     def __init__(self, y):
         self.x = 200
-        self.y = y
+        self.y = y + 20
         self.frames = FIREBALL_FRAMES
         self.width, self.height = (self.frames[0].get_width(), self.frames[0].get_height())
         self.animation_timer = 0
@@ -13,7 +13,7 @@ class Fireball():
         self.masks = [pygame.mask.from_surface(image) for image in self.frames]
 
     def update(self):
-        self.x += 2
+        self.x += 5
         self.animation_timer += clock.get_time()
         self.current_frame = int(self.animation_timer / 100) % len(self.frames)
 
@@ -25,16 +25,16 @@ class Fireball():
             screen.blit(mask_surface, (self.x, self.y))
 
     def complete_off_screen(self):
-        return self.x + self.width > SCREEN_WIDTH
+        return self.x + self.width > SCREEN_WIDTH + 200
 
     def collides_with(self, obstacle):
         if obstacle.type == "double":
-            obstacle_1 = obstacle.mask.overlap(self.masks[self.current_frame], (self.x - self.x, self.y - self.y[0]))
+            obstacle_1 = obstacle.mask.overlap(self.masks[self.current_frame], (self.x - obstacle.x, self.y - obstacle.y[0]))
             obstacle_2 = obstacle.mask.overlap(self.masks[self.current_frame],
-                                           (self.x - self.x - obstacle.width[0] - 10, self.y - self.y[1]))
+                                           (self.x - obstacle.x - obstacle.width[0] - 10, self.y - obstacle.y[1]))
             return obstacle_1 is not None or obstacle_2 is not None
         elif obstacle.type == "bird":
             return obstacle.masks[obstacle.current_frame].overlap(self.masks[self.current_frame],
-                                                          (self.x - self.x, self.y - self.y[0])) is not None
+                                                          (self.x - obstacle.x, self.y - obstacle.y[0])) is not None
         else:
-            return obstacle.mask.overlap(self.masks[self.current_frame], (self.x - self.x, self.y - self.y[0])) is not None
+            return obstacle.mask.overlap(self.masks[self.current_frame], (self.x - obstacle.x, self.y - obstacle.y[0])) is not None
